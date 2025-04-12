3ecf097104e875191a524007d35ac6912c2f4cd9
zyril <zyrillelliryz@gmail,com>
2025-04-12 16:31:42 +0800

    update config

--- helpcenter/next.config.ts
+++ helpcenter/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['proj3helpcenter.s3.ap-southeast-1.amazonaws.com'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
